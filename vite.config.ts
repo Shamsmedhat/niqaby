ac2f8d32fb0322fa3fdad67c2db0e33e5f6515db
Shams <shamsmedhat1@gmail.com>
2025-03-02 04:51:17 +0200

    sec commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import themePlugin from "@replit/vite-plugin-shadcn-theme-json";
 import path, { dirname } from "path";
-import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
@@ -10,18 +8,9 @@ const __dirname = dirname(__filename);
 
 export default defineConfig({
   plugins: [
-    react(),
-    runtimeErrorOverlay(),
-    themePlugin(),
-    ...(process.env.NODE_ENV !== "production" &&
-    process.env.REPL_ID !== undefined
-      ? [
-          await import("@replit/vite-plugin-cartographer").then((m) =>
-            m.cartographer(),
-          ),
-        ]
-      : []),
+    react()
   ],
+  base: "/Niqaby/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "client", "src"),
@@ -30,7 +19,6 @@ export default defineConfig({
   },
   root: path.resolve(__dirname, "client"),
   build: {
-    outDir: path.resolve(__dirname, "dist/public"),
-    emptyOutDir: true,
-  },
+    outDir: "dist",
+  }
 });
